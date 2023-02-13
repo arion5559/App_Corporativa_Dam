cf33d79ff7d3236cf2a01bacbd9c0f3d1cafa3a1
arion5559 <42389714+arion5559@users.noreply.github.com>
2023-02-13 20:21:27 +0100

    Intento de hacer una animación en clase (está en proceso, más o menos)

---
+++ app/src/main/java/com/example/appcorporativa/StartController.java
@@ -0,0 +1,10 @@
+package com.example.appcorporativa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class StartController extends AppCompatActivity {
+    @Override
+    public void onCreate(){
+        super.onCreate();
+    }
+}
