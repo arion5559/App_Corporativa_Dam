8a7dd794ed4f72d921eeb62cabcbbf0511d7a276
arion5559 <42389714+arion5559@users.noreply.github.com>
2023-03-06 13:01:21 +0100

    Intento de hacer una animación en clase (está en proceso, más o menos)

--- app/src/main/java/com/example/appcorporativa/StartController.java
+++ app/src/main/java/com/example/appcorporativa/StartController.java
@@ -1,10 +1,26 @@
 package com.example.appcorporativa;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class StartController extends AppCompatActivity {
     @Override
-    public void onCreate(){
-        super.onCreate();
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.start);
+
+        ImageView imageView = findViewById(R.id.start);
+
+        imageView.setOnTouchListener(new OnSwipeTouchListener(this) {
+            @Override
+            public void onSwipeTop() {
+                super.onSwipeTop();
+                Intent intent = new Intent(StartController.this, LogInController.class);
+                startActivity(intent);
+            }
+        });
     }
 }
