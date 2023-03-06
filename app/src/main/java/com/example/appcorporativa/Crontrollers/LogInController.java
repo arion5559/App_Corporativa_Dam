81eca47b3a0c2790f6fc5d28904d5642f187f4b5
arion5559 <42389714+arion5559@users.noreply.github.com>
2023-03-06 23:28:15 +0100

    Actualización de pantallas y añadido de permisos para el funcionamiento

--- app/src/main/java/com/example/appcorporativa/Crontrollers/LogInController.java
+++ app/src/main/java/com/example/appcorporativa/Crontrollers/LogInController.java
@@ -2,19 +2,50 @@ package com.example.appcorporativa.Crontrollers;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.example.appcorporativa.DatabaseHandlers.DbUsuarios;
+import com.example.appcorporativa.Modelo.Usuario;
 import com.example.appcorporativa.R;
 
 public class LogInController extends AppCompatActivity {
+
+    private EditText username;
+    private EditText password;
+    private Button login;
+    private Button register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-    }
 
-    public void logIn() {
+        username = findViewById(R.id.et_username);
+        password = findViewById(R.id.et_password);
+        login = findViewById(R.id.btn_login);
+        register = findViewById(R.id.btn_register);
 
+        login.setOnClickListener(v -> {
+            String username = this.username.getText().toString();
+            String password = this.password.getText().toString();
+
+            if (username.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "Todos los campos son obligatorios", Toast.LENGTH_SHORT).show();
+            } else {
+                DbUsuarios dbUsuarios = new DbUsuarios(this);
+                Usuario usuario = dbUsuarios.logIn(username, password);
+                if (usuario != null) {
+                    Toast.makeText(this, "Bienvenido " + usuario.getNombre(), Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(this, MainActivity.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(this, "Usuario o contraseña incorrectos", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/appcorporativa/Crontrollers/RegisterController.java
+++ app/src/main/java/com/example/appcorporativa/Crontrollers/RegisterController.java
@@ -2,15 +2,64 @@ package com.example.appcorporativa.Crontrollers;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.example.appcorporativa.DatabaseHandlers.DbHelper;
+import com.example.appcorporativa.DatabaseHandlers.DbUsuarios;
+import com.example.appcorporativa.Modelo.Usuario;
 import com.example.appcorporativa.R;
 
 public class RegisterController extends AppCompatActivity {
+
+    private EditText nombre;
+    private EditText username;
+    private EditText email;
+    private EditText password;
+    private EditText password2;
+    private Button registrar;
+    private Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+        nombre = findViewById(R.id.et_name);
+        username = findViewById(R.id.et_username);
+        email = findViewById(R.id.et_email);
+        password = findViewById(R.id.et_password);
+        password2 = findViewById(R.id.et_confirm_password);
+        registrar = findViewById(R.id.btn_register);
+        login = findViewById(R.id.btn_login);
+
+        registrar.setOnClickListener(v -> {
+            String nombre = this.nombre.getText().toString();
+            String username = this.username.getText().toString();
+            String email = this.email.getText().toString();
+            String password = this.password.getText().toString();
+            String password2 = this.password2.getText().toString();
+
+            if (nombre.isEmpty() || username.isEmpty() || email.isEmpty() || password.isEmpty() || password2.isEmpty()) {
+                Toast.makeText(this, "Todos los campos son obligatorios", Toast.LENGTH_SHORT).show();
+            } else if (!password.equals(password2)) {
+                Toast.makeText(this, "Las contraseñas no coinciden", Toast.LENGTH_SHORT).show();
+            } else {
+                DbUsuarios dbUsuarios = new DbUsuarios(this);
+                dbUsuarios.registrarUsuario(nombre, username, email, password);
+                Toast.makeText(this, "Usuario registrado correctamente", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(this, LogInController.class);
+                startActivity(intent);
+            }
+        });
+
+        login.setOnClickListener(v -> {
+            Intent intent = new Intent(this, LogInController.class);
+            startActivity(intent);
+        });
     }
 }
